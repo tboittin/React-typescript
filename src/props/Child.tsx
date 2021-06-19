7ff0a4a803a147a19f5db4ac293ef2629ec571b1
Thomas Boittin <tboittin@gmail.com>
2021-06-19 15:59:15 +0200

    9. Annotations with Children

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,23 @@
 interface ChildProps {
   color: string
+  onClick: () => void
 }
 
-export const Child = ({color}: ChildProps) => {
-  return <h1>{color}</h1>
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <h1>
+      {color}
+      <button onClick={onClick}>Click Me</button>
+    </h1>
+  )
+}
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color, onClick, children }) => {
+  return (
+    <h1>
+      {color}
+      {children}
+      <button onClick={onClick}>Click Me</button>
+    </h1>
+  )
 }
