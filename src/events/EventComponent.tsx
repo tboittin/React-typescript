75bc4889047facbfff1ab71c831fe8b34b5ac751
Thomas Boittin <tboittin@gmail.com>
2021-06-21 12:42:38 +0200

    16. Typing Standalone Event Handlers

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,12 @@
+const EventComponent: React.FC = () => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event)
+  }
+  return (
+    <div>
+      <input onChange={onChange} />
+    </div>
+  )
+}
+
+export default EventComponent
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import  ReactDOM  from "react-dom";
+import EventComponent from "./events/EventComponent";
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
+
 
 const App = () => {
   return (
     <div>
       {/* <GuestList /> */}
-      <UserSearch />
+      {/* <UserSearch /> */}
+      <EventComponent />
     </div>
   )
 }
